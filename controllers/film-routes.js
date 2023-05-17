04e42f1671d33b88d8e0699ba4caaccfcf795f75
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-05-17 18:15:23 +0100

    updated post review route

--- controllers/film-routes.js
+++ controllers/film-routes.js
@@ -307,7 +307,7 @@ router.post("/reviews", async (req, res) => {
       user_id: req.user.user_id,
     });
 
-    return res.status(201).json({ message: "Review added successfully" });
+    return res.redirect("/dashboard")
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Server error" });
